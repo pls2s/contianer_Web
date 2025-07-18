9f7423f782b078d09a22ef3b25d7df624457c76a
peerawoot <peerawoot1767@gmail.com>
2025-07-18 21:47:55 +0700

    Login Font End

--- webpage/views.py
+++ webpage/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home (request):
     return render(request,"home.html")
+
+def login (request):
+    return render(request,"login.html")
